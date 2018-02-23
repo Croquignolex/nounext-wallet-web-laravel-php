14d16f6436dd929239fe7940ee048bf56a1dc104
Alex NGOMBOL <gpetitalex10@gmail.com>
2018-02-23 22:14:44 +0100

    Adjuster login and error layout for mobile

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -5,24 +5,25 @@
 @endsection
 
 @section('body')
-    @include('partials.language.language', ['style' => 'lang'])
-    <div class="row justify-content-center" style="padding-top: 5%">
-        <div class="card app-main-border">
-            <div class="card-header text-center app-main-bg"><h4 class=" text-uppercase">{{ config('company.name') }} <strong>{{ config('app.name')}}</strong></h4></div>
-            <div class="card-body">
-                <h1 class="card-title app-main-color text-uppercase">{{ __($page) }}</h1>
-                @yield('error_title').<br />
-                @yield('error_message').<br /><br />
-                @lang('errors.return').<br />
-                <a href="{{ route_manager('home') }}" class="btn app-main-btn">
-                    <span class="oi oi-home"></span>&nbsp;
-                    @lang('general.home')
-                </a>
+    <div class="container-fluid">
+        @include('partials.language.language', ['style' => 'lang'])
+        <div class="row justify-content-center" style="padding-top: 50px">
+            <div class="card app-main-border">
+                <div class="card-header text-center app-main-bg"><h4 class=" text-uppercase">{{ config('company.name') }} <strong>{{ config('app.name')}}</strong></h4></div>
+                <div class="card-body">
+                    <h1 class="card-title app-main-color text-uppercase">{{ __($page) }}</h1>
+                    @yield('error_title').<br />
+                    @yield('error_message').<br /><br />
+                    @lang('errors.return').<br />
+                    <a href="{{ route_manager('home') }}" class="btn app-main-btn">
+                        <span class="oi oi-home"></span>&nbsp;
+                        @lang('general.home')
+                    </a>
+                </div>
             </div>
-        </div> 
+        </div>
+        @include('partials.footer')
     </div>
-
-    @include('partials.footer')
 @endsection
 
 @push('style.layout')
